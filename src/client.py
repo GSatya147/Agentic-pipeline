ef3b97d43e6e41091d93401a3f56b7a3fd05b1c6
GSatya147 <gvvsn147@gmail.com>
2026-06-21 12:46:26 +0100

    code: add client, litellm routing

--- src/client.py
+++ src/client.py
@@ -1,11 +1,20 @@
+import os
+
 from dotenv import load_dotenv
 import litellm
 
 load_dotenv()
 
 class Client:
-    def __init__(self):
-        pass
+    def __init__(self, model):
+        self.model = model
 
-    def call_llm(self):
-        pass
+    def call_llm(self, messages, tools):
+        response = litellm.completion(
+            api_key=os.getenv("DEEPSEEK_API_KEY"),
+            model=self.model,
+            messages=messages,
+            tools=tools,
+        )
+
+        return response.choices[0].message
