f8127bf605ecd1deb5dabc88ba2bd2980c101c61
GSatya147 <gvvsn147@gmail.com>
2026-06-23 11:35:00 +0100

    code: add main entry point

---
+++ main.py
@@ -0,0 +1,23 @@
+
+from src.tools import Tools
+from src.graph import AgentGraph
+
+user_query = input(">> ")
+
+tool = Tools()
+tools_schema = tool.add_tool_schema()
+
+initial_state = {
+    "messages" : [{
+        "role" : "user",
+        "content" : user_query
+    }],
+    "tools_schema" : tools_schema,
+    "step_count" : 0
+}
+graph = AgentGraph()
+try:
+    graph.invoke_graph(initial_state=initial_state)
+
+except Exception as e:
+    print(e)
