7c9cc7dbd90c35f337b9b0588e46100d91ff2e3b
GSatya147 <gvvsn147@gmail.com>
2026-06-24 12:18:22 +0100

    code: add tool node, remove individual tools nodes

--- src/nodes.py
+++ src/nodes.py
@@ -21,27 +21,7 @@ def llm_call(state: AgentState) -> dict:
         response    : {response.get("content", "Execute search tool")} 
     """)
 
-    return {"messages" : [response], "step_count" : state["step_count"] + 1}
-
-def search_tool(state: AgentState):
-    tool_obj = Tools()
-
-    search_query = state["messages"][-1].tool_calls[0]["args"]["query"]
-    tool_call_id = state["messages"][-1].tool_calls[0]["id"]
-    search_result = tool_obj.search_tool(query=search_query)
-
-    tool_message = {
-        "role" : "tool",
-        "content" : search_result,
-        "tool_call_id" : tool_call_id
-    }
-
-    print(f"""
-        Search Node Executing...
-        response : {search_result} 
-    """)    
-
-    return {"messages" : [tool_message], "step_count" : state["step_count"] + 1}
+    return {"messages" : [response], "step_count" : state["step_count"] + 1}   
 
 def final_node(state: AgentState):
     print("Final Node Executing...")
@@ -51,6 +31,54 @@ def final_node(state: AgentState):
     
     else:
         print(f"        Final Result: {state["messages"][-1].content}")
+    
+def tool_node(state: AgentState):
+    print("Tool Node Executing")
+    # print(state["messages"][-1].tool_calls[0])
+    tool_name = state["messages"][-1].tool_calls[0]["name"]
+
+    tool_obj = Tools()
+
+    if tool_name == "calculator_tool":
+
+        expression_query = state["messages"][-1].tool_calls[0]["args"]["expression"]
+        tool_call_id = state["messages"][-1].tool_calls[0]["id"]
+        expression_result = tool_obj.calculator_tool(query=expression_query)
+
+        tool_message = {
+            "role" : "tool",
+            "content" : expression_result,
+            "tool_call_id" : tool_call_id
+        }
+
+        print(f"""
+            Calculator Executing...
+            response : {expression_result} 
+        """)    
+
+        return {"messages" : [tool_message], "step_count" : state["step_count"] + 1}
+
+    elif tool_name == "search_tool":
+
+        search_query = state["messages"][-1].tool_calls[0]["args"]["query"]
+        tool_call_id = state["messages"][-1].tool_calls[0]["id"]
+        search_result = tool_obj.search_tool(query=search_query)
+
+        tool_message = {
+            "role" : "tool",
+            "content" : search_result,
+            "tool_call_id" : tool_call_id
+        }
+
+        print(f"""
+            Web search Executing...
+            response : {search_result} 
+        """)    
+
+        return {"messages" : [tool_message], "step_count" : state["step_count"] + 1}
+
+    else:
+        return {"messages" : [{"role" : "tool", "content" : "Error, unknow tool requested [use search_tool/calculator_tool]"}], "step_count" : state["step_count"] + 1}
 
 def routing_logic(state: AgentState):
     print("Routing Node Executing...")
@@ -60,8 +88,8 @@ def routing_logic(state: AgentState):
         return "final_node"
     
     if response:
-        print(f"        Action: To Execute {response[0]["name"]}, routed towards search node")
-        return "search_tool"
+        print(f"        Action: To Execute {response[0]["name"]}, routed towards tool node")
+        return "tool_node"
     else:
         print(f"        Action: routed towards final node")
         return "final_node"
