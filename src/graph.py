61e87e7d2fbfa3c0d5bcc52cfe4006ec3d973fa0
GSatya147 <gvvsn147@gmail.com>
2026-06-24 12:19:23 +0100

    code: add tool_node, edges between llm node and tool node

--- src/graph.py
+++ src/graph.py
@@ -2,7 +2,7 @@ from langgraph.graph import StateGraph, START, END
 from langgraph.checkpoint.memory import InMemorySaver 
 
 from src.state import AgentState
-from src.nodes import llm_call, search_tool, routing_logic, final_node
+from src.nodes import llm_call, tool_node, routing_logic, final_node
 
 class AgentGraph:
     def __init__(self):
@@ -10,12 +10,12 @@ class AgentGraph:
         self.checkpointer = InMemorySaver()
         # add nodes
         self.graph.add_node("llm_call", llm_call)
-        self.graph.add_node("search_tool", search_tool)
+        self.graph.add_node("tool_node", tool_node)
         self.graph.add_node("final_node", final_node)
 
         # add edges
         self.graph.add_edge(START, "llm_call")
-        self.graph.add_edge("search_tool", "llm_call")
+        self.graph.add_edge("tool_node", "llm_call")
         self.graph.add_edge("final_node", END)
 
         # add conditional edges
