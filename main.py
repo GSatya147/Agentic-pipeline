0e0e2a8371ff277905f83d3d6fa36e3e669ea0a6
GSatya147 <gvvsn147@gmail.com>
2026-06-24 11:26:47 +0100

    code: add history command, configurable

--- main.py
+++ main.py
@@ -2,23 +2,34 @@
 from src.tools import Tools
 from src.graph import AgentGraph
 
-user_query = input(">> ")
-
 tool = Tools()
 tools_schema = tool.add_tool_schema()
 
-initial_state = {
-    "messages" : [{
-        "role" : "user",
-        "content" : user_query
-    }],
-    "tools_schema" : tools_schema,
-    "step_count" : 0
-}
 graph = AgentGraph()
 
-try:
-    graph.invoke_graph(initial_state=initial_state)
+while True:
+    thread_id = int(input("enter numerical id: "))
+    user_query = input(">> ")
 
-except Exception as e:
-    print(e)
+    initial_state = {
+        "messages" : [{
+            "role" : "user",
+            "content" : user_query
+        }],
+        "tools_schema" : tools_schema,
+        "step_count" : 0,
+    }
+    try:
+        if user_query.lower() == "history":
+            graph.get_history(thread_id=thread_id)
+
+        else:
+            graph.invoke_graph(user_query=user_query, initial_state=initial_state, thread_id=thread_id)
+
+    except KeyboardInterrupt as k:
+        print(k)
+        break
+
+    except Exception as e:
+        print(e)
+        break
--- src/graph.py
+++ src/graph.py
@@ -1,4 +1,5 @@
 from langgraph.graph import StateGraph, START, END
+from langgraph.checkpoint.memory import InMemorySaver 
 
 from src.state import AgentState
 from src.nodes import llm_call, search_tool, routing_logic, final_node
@@ -6,6 +7,7 @@ from src.nodes import llm_call, search_tool, routing_logic, final_node
 class AgentGraph:
     def __init__(self):
         self.graph = StateGraph(AgentState)
+        self.checkpointer = InMemorySaver()
         # add nodes
         self.graph.add_node("llm_call", llm_call)
         self.graph.add_node("search_tool", search_tool)
@@ -19,11 +21,36 @@ class AgentGraph:
         # add conditional edges
         self.graph.add_conditional_edges("llm_call", routing_logic)
 
-        self.graph = self.graph.compile()
+        self.graph = self.graph.compile(checkpointer=self.checkpointer)
 
-    def invoke_graph(self, initial_state):
+    def invoke_graph(self, user_query, initial_state, thread_id):
+        config = {
+            "configurable" : {
+                "thread_id" : f"thread_{thread_id}"
+            }
+        }
+
         try:
-            self.graph.invoke(initial_state)
+            snapshot = self.graph.get_state(config=config)
+
+            if snapshot.values:
+                self.graph.invoke({"messages" : [{"role" : "user", "content" : user_query}]}, config)
+
+            else:
+                self.graph.invoke(initial_state, config)
 
         except Exception as e:
             print(e)
+
+    def get_history(self, thread_id):
+        config = {
+            "configurable" : {
+                "thread_id" : f"thread_{thread_id}"
+            }
+        }
+
+        if config:
+            snapshot = self.graph.get_state(config=config)
+
+            if snapshot.values:
+                print(f"History: {snapshot.values["messages"]}")
