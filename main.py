ee99497c46ce074718c0b7c20087af9bfec0298d
GSatya147 <gvvsn147@gmail.com>
2026-06-24 13:11:33 +0100

    code: add HITL both approving and modification states

--- main.py
+++ main.py
@@ -24,7 +24,19 @@ while True:
             graph.get_history(thread_id=thread_id)
 
         else:
-            graph.invoke_graph(user_query=user_query, initial_state=initial_state, thread_id=thread_id)
+            snapshot = graph.invoke_graph(user_query=user_query, initial_state=initial_state, thread_id=thread_id)
+
+            if "final_node" in snapshot.next:
+                print(f"Draft answer: {snapshot.values["messages"][-1].content}")
+
+                user_pref = input(">> Approve? (y/n)")
+
+                if user_pref.lower() == "y":
+                    graph.resume_graph(thread_id)
+
+                elif user_pref.lower() == "n":
+                    modification = input(">> modified content: ")
+                    graph.modify_and_resume(modification, thread_id)
 
     except KeyboardInterrupt as k:
         print(k)
